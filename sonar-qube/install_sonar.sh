dd5274c8c5f8bae6a69a0a4ec62e8109ec84f7e7
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-04-08 10:38:43 -0300

    fix: remove directory from terraform init

---
+++ sonar-qube/install_sonar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker volume create --name sonarqube_data
+docker volume create --name sonarqube_logs
+docker volume create --name sonarqube_extensions
+
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
--- sonar-qube/main.tf
+++ sonar-qube/main.tf
@@ -2,25 +2,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_security_group" "sg_8080" {
+  name = "terraform-learn-state-sg-8080"
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "9000"
+    to_port     = "9000"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
+resource "aws_instance" "ec2_instance" {
+  ami = "ami-080e1f13689e07408"
+  instance_type = "t2.medium"
   tags = {
     Name = "sonarqube-instance"
   }
-
-  key_name = "nome-da-chave-ssh"
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y openjdk-11-jdk
-    wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-88267.zip
-    sudo unzip sonarqube-88267.zip -d /opt
-    sudo mv /opt/sonarqube-88267 /opt/sonarqube
-    sudo /opt/sonarqube/bin/linux-x86-64/sonar.sh start
-    EOF
+  vpc_security_group_ids = [aws_security_group.sg_8080.id]
+  key_name = "deployment-server"
+  user_data = file("install_sonar.sh")
 }
 
 output "public_ip" {
